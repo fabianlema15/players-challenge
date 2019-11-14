0e2785a316a769e2864ef6abd844ca66aaca5d64
Fabian Lema <fabianlemac@gmail.com>
2019-11-14 12:09:36 -0600

    first conf

--- serve.js
+++ serve.js
@@ -8,8 +8,8 @@ app.use('/styles', express.static('node_modules/bootstrap/dist/css'));
 app.use('/scripts', express.static('node_modules/bootstrap/dist/js'));
 
 // Setup routes
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/mock.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/frontend/mock.html'));
 });
 
 // start app
